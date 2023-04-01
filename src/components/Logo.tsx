58c23ffb93840d698100ea48007c94f9912576d2
208147348 <208147348@idf.il>
2023-04-01 13:09:30 +0300

    fix structure

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,14 +2,14 @@ import "../styles/LogoSection.css"
 
 function Logo(){
     return (
-        <div>
+        <section className="logoSection">
             <div className="logo">
                 <img src={process.env.PUBLIC_URL + '/ReadMyLipsLogo.png'} alt="ReadMyLips Inc. logo"/>          
             </div>
             <div className="sub-heading">
                 <p id="title">We turn lip movements into text.</p>
             </div>
-        </div>
+        </section>
     );
 }
 
--- src/components/UploadSection.tsx
+++ src/components/UploadSection.tsx
@@ -1,8 +1,12 @@
+import "../styles/UploadSection.css"
+
 function UploadSection(){
     return (
-        <section>
-            <button>Add video</button>
-            <h4>It doesn't have to contain sound :)</h4>
+        <section id="upload-section">
+            <div>
+                <button id="upload-button">Add video</button>
+                <h4>It doesn't have to contain sound :)</h4>
+            </div>
         </section>
     );
 }
