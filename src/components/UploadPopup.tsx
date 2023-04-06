2a4f410c5e7b7b4d1b35f453c0f9601dcc0c63a6
Daniel-Toplian <danieltoplian@gmail.com>
2023-04-06 20:31:20 +0300

    upload popup

--- src/components/UploadPopup.tsx
+++ src/components/UploadPopup.tsx
@@ -1,5 +1,45 @@
-function UploadPopup() {
-  return <div></div>
+import UploadOption from './UploadOption'
+import '../styles/UploadVideo.css'
+
+type PopupProp = {
+  trigger: boolean
+  setTrigger: (value: boolean) => void
 }
+
+function UploadPopup(props: PopupProp) {
+  return props.trigger ? (
+    <div className='upload-backgournd'>
+      <div className='popup-block'>
+        <button className='exit-button' onClick={() => props.setTrigger(false)}>
+          X
+        </button>
+        <div className='options-container'>
+          <div className='upload-option'>
+            <UploadOption
+              title='Upload from computer'
+              imagePath={process.env.PUBLIC_URL + 'computerIcon.png'}
+              alt='Upload from computer icon'
+              handleClickEvent={uploadFromComputerAction}
+            />
+          </div>
+          <div className='upload-option'>
+            <UploadOption
+              title='Film video'
+              imagePath={process.env.PUBLIC_URL + 'camIcon.png'}
+              alt='Film video icon'
+              handleClickEvent={filmVideoAction}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div></div>
+  )
+}
+
+function uploadFromComputerAction(): void {}
+
+function filmVideoAction(): void {}
 
 export default UploadPopup
